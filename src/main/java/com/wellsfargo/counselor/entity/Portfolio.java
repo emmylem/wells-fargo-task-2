8e9917fdb55e2ac971dd7924f9ae96219307a693
emmylem <benjaminosondu55@gmail.com>
2024-03-01 19:48:23 +0100

    Portfolio object associated with a client

--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -1,2 +1,21 @@
-package com.wellsfargo.counselor.entity;public class Portfolio {
+package com.wellsfargo.counselor.entity;
+
+import javax.persistence.*;
+@Entity
+public class Portfolio {
+    import javax.persistence.*;
+
+    @Entity
+    public class Portfolio {
+
+        @Id
+        @GeneratedValue(strategy=GenerationType.AUTO)
+        private int id;
+
+        @OneToOne
+        @JoinColumn(name="client_id")
+        private Client client;
+
+        // constructor and getters/setters
+
 }
