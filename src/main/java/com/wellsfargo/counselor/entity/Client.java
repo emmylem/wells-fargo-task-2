c67668a1c9bbeae02117c21a171cb59ca55c8de5
emmylem <benjaminosondu55@gmail.com>
2024-03-01 19:46:47 +0100

    Client object with @ManyToOne relationship

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -1,2 +1,30 @@
-package com.wellsfargo.counselor.entity;public class Client {
+package com.wellsfargo.counselor.entity;
+
+import javax.persistence.*;
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String contactInfo;
+
+    @ManyToOne
+    @JoinColumn(name="advisor_id")
+    private FinancialAdvisor advisor;
+
+    public Client() {}
+
+    public Client(String name, String contactInfo, FinancialAdvisor advisor) {
+        this.name = name;
+        this.contactInfo = contactInfo;
+        this.advisor = advisor;
+    }
+
+    // getters and setters
+
 }
