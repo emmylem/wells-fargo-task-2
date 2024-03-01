7519498d5ca1a34da5b84d6a17f5661185f11278
emmylem <benjaminosondu55@gmail.com>
2024-03-01 19:49:38 +0100

    Security object, likely representing financial instruments held in a portfolio

--- src/main/java/com/wellsfargo/counselor/entity/Security.java
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -1,2 +1,15 @@
-package com.wellsfargo.counselor.entity;public class Security {
+package com.wellsfargo.counselor.entity;
+
+import javax.persistence.*;
+@Entity
+public class Security {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="portfolio_id")
+    private Portfolio portfolio;
+
+    // other fields, constructor, getters/setters
 }
